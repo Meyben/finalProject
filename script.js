fbd8b2b8bc1f0cb3af0696d0ebe1d44db2df2aa1
Meyben <ida@honningbier.no>
2016-11-04 13:29:28 +0100

    newidea!

--- script.js
+++
@@ -1,8 +0,0 @@
-function navFunction() {
-    var Bob = document.getElementById("NavId");
-    if (Bob.className === "navbar") {
-        Bob.className += " responsive";
-    } else {
-        Bob.className = "navbar";
-    }
-}
